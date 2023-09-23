4a6196fa7f4d13153bd02d1cb657daed21fdac1d
LauraYarda <laurayarda5@gmail>
2023-09-23 11:40:21 +0100

    file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+#the coode will create a file name school inside the /tmp directory 
 file { '/tmp/school':
   ensure  => file,
-  mode    => '0744',
+  mode    => '07744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
